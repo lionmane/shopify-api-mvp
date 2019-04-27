cdd2049b400709f5d043b530b86127a22491ff49
Mario J. Wunderlich <mario@lionmane.io>
2019-04-26 21:38:13 -0600

    Added resources to main layout used by the stripe integration

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -25,6 +25,8 @@
               integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp"
               crossorigin="anonymous">
 
+        <link rel="stylesheet" href="/css/stripe.css">
+
         @yield('styles')
 
     <!-- Latest compiled and minified JavaScript -->
@@ -35,7 +37,8 @@
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
                 integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
                 crossorigin="anonymous"></script>
-
+        <script src="https://js.stripe.com/v3/"></script>
+        <script src="https://js.braintreegateway.com/js/braintree-2.32.1.min.js"></script>
         @yield('scripts')
     </head>
     <body>
